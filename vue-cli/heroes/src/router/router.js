e1047c0dbbbe6b39087686dcd774957becb127d4
sukai <csk199623@icloud.com>
2021-01-25 19:53:36 +0800

    完成增删改查

--- vue-cli/heroes/src/router/router.js
+++ vue-cli/heroes/src/router/router.js
@@ -9,11 +9,15 @@ Vue.use(VueRouter)
 import List from '../components/list/list.vue'
 import Bar from '../components/bar/bar.vue'
 import Foo from '../components/foo/foo.vue'
+import Add from '../components/list/add.vue'
+import Edit from '../components/list/edit.vue'
 
 const routes = [
   {name: 'heroes', path: '/heroes', component: List},
   {name: 'bar', path: '/bar', component: Bar},
-  {name: 'foo', path: '/foo', component: Foo}
+  {name: 'foo', path: '/foo', component: Foo},
+  {name: 'add', path: '/add', component: Add},
+  {name: 'edit', path: '/edit/:id', component: Edit}
 ]
 
 /*实例化路由*/
