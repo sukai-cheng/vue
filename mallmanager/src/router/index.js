b465f22a9a1993b2904e5b047f8a7eff28077361
sukai <csk199623@icloud.com>
2021-01-27 07:17:53 +0800

    users组件

--- mallmanager/src/router/index.js
+++ mallmanager/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Login from '@/components/login/login'
 import Home from '@/components/home/home'
+import User from '@/components/users/users'
 import Router from 'vue-router'
 
 Vue.use(Router)
@@ -15,7 +16,15 @@ export default new Router({
     {
       name:'home',
       path: '/home',
-      component: Home
-    }
+      component: Home,
+      children:[
+        {
+          name:'users',
+          path:'/users',
+          component: User
+        }
+      ]
+    },
+
   ]
 })
