03421877de7d9544038a018bbf24bd0a8dc96f3a
sukai <csk199623@icloud.com>
2021-02-18 07:23:38 +0800

    商品管理-添加商品

--- mallmanager/src/components/goods/goodsadd.vue
+++ mallmanager/src/components/goods/goodsadd.vue
@@ -23,18 +23,79 @@
       <el-step title="商品图片"></el-step>
       <el-step title="商品内容"></el-step>
     </el-steps>
-    <!-- 切换标签 -->
-    <el-tabs
-      v-model="active"
-      tab-position="left"
-      style="height:200px;margin-top:30px"
+    <!-- 表单 -->
+    <el-form
+      label-position="top"
+      label-width="80px"
+      :model="form"
+      style="height:400px;overflow:auto;"
     >
-      <el-tab-pane name="1" label="商品参数">商品参数</el-tab-pane>
-      <el-tab-pane name="2" label="基本信息">基本信息</el-tab-pane>
-      <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
-      <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
-      <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
-    </el-tabs>
+      <!-- 切换标签 -->
+      <el-tabs
+        @tab-click="tabChange()"
+        v-model="active"
+        tab-position="left"
+        style="margin-top:20px"
+      >
+        <el-tab-pane name="1" label="商品参数">
+          <!-- 显示的是该三级分类的商品参数 -->
+          <el-form-item
+            :label="item1.attr_name"
+            :key="i"
+            v-for="(item1, i) in arrDyparams"
+          >
+            <!-- 复选框组 -->
+            <el-checkbox-group v-model="item1.attr_vals">
+              <el-checkbox
+                border
+                v-for="(item2, i) in item1.attr_vals"
+                :key="i"
+                :label="item2"
+              ></el-checkbox>
+            </el-checkbox-group>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="2" label="基本信息">
+          <el-form-item label="商品名称">
+            <el-input v-model="form.goods_name"></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格">
+            <el-input v-model="form.goods_price"></el-input>
+          </el-form-item>
+          <el-form-item label="商品重量">
+            <el-input v-model="form.goods_weight"></el-input>
+          </el-form-item>
+          <el-form-item label="商品数量">
+            <el-input v-model="form.goods_number"></el-input>
+          </el-form-item>
+
+          <el-form-item label="商品分类">
+            <!-- 级联显示框 -->
+            <el-cascader
+              clearable
+              expand-trigger="hover"
+              :options="options"
+              v-model="selectedOptions"
+              @change="handleChange"
+              :props="defaultProps"
+            >
+            </el-cascader>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="3" label="商品属性">
+          <!-- 静态参数的数据 -->
+          <el-form-item
+            :label="item.attr_name"
+            v-for="(item, i) in arrStaticparams"
+            :key="i"
+          >
+            <el-input v-model="item.attr_vals"></el-input>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
+        <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
+      </el-tabs>
+    </el-form>
   </el-card>
 </template>
 
@@ -43,8 +104,75 @@ import myBread from "../custom/myBread.vue";
 export default {
   data() {
     return {
-      active: "1"
+      arrStaticparams: [],
+      active: "1",
+      // id
+      // goods_name
+      // goods_price
+      // goods_number
+      // goods_weight
+      // goods_introduce
+      // pics
+      // attrs
+      form: {
+        goods_name: "",
+        goods_price: "",
+        goods_number: "",
+        goods_weight: "",
+        goods_introduce: "",
+        pics: "",
+        attrs: ""
+      },
+      arrDyparams: [],
+      // 级联选择器绑定的数据
+      options: [],
+      selectedOptions: [1, 3, 6],
+      defaultProps: {
+        label: "cat_name",
+        value: "cat_id",
+        children: "children"
+      }
     };
+  },
+  created() {
+    this.getGoodCate();
+  },
+  methods: {
+    // 点击不同的tabs触发方法
+    async tabChange() {
+      if (this.active === "1") {
+        if (this.selectedOptions.length !== 3) {
+          this.$message.warning("请先选择商品的三级分类");
+          return;
+        }
+        const res = await this.$http.get(
+          //manys表示动态参数的数据
+          `categories/${this.selectedOptions[2]}/attributes?sel=many`
+        );
+        this.arrDyparams = res.data.data;
+        this.arrDyparams.forEach(item => {
+          item.attr_vals.length === 0
+            ? []
+            : (item.attr_vals = item.attr_vals.trim().split(","));
+        });
+      } else if (this.active === "3") {
+        if (this.selectedOptions.length !== 3) {
+          this.$message.warning("请先选择商品的三级分类");
+          return;
+        }
+        //获取静态参数的数据
+        const res = await this.$http.get(
+          //only表示动态参数的数据
+          `categories/${this.selectedOptions[2]}/attributes?sel=only`
+        );
+        this.arrStaticparams = res.data.data;
+      }
+    },
+    handleChange() {},
+    async getGoodCate() {
+      const res = await this.$http.get(`categories?type=3`);
+      this.options = res.data.data;
+    }
   },
   components: { myBread }
 };
