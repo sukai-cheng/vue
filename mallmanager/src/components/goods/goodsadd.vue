8c19347ff1ec85b7f15fb081cfcfb00b35767837
sukai <csk199623@icloud.com>
2021-03-04 00:54:17 +0800

    完善添加商品功能
    1. 商品图片上传
    2. 新增富文本组件
    3. 格式化对象满足API规范

--- mallmanager/src/components/goods/goodsadd.vue
+++ mallmanager/src/components/goods/goodsadd.vue
@@ -92,8 +92,30 @@
             <el-input v-model="item.attr_vals"></el-input>
           </el-form-item>
         </el-tab-pane>
-        <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
-        <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
+        <el-tab-pane name="4" label="商品图片">
+          <el-form-item>
+            <!-- baseUrl axios发的请求 -->
+            <el-upload
+              action="http://127.0.0.1:8888/api/private/v1/upload"
+              :on-preview="handlePreview"
+              :on-remove="handleRemove"
+              :on-success="handleSuccess"
+              :headers="headers"
+              list-type="picture"
+            >
+              <el-button size="small" type="primary">点击上传</el-button>
+              <div slot="tip" class="el-upload__tip">只能上传jpg/png格式</div>
+            </el-upload>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="5" label="商品内容">
+          <el-form-item>
+            <!-- 表单元素 -->
+            <el-button type="primary" @click="addGoods">添加商品</el-button>
+            <!-- 富文本编辑器(需要用到富文本) -->
+            <quill-editor v-model="form.goods_introduce"></quill-editor>
+          </el-form-item>
+        </el-tab-pane>
       </el-tabs>
     </el-form>
   </el-card>
@@ -101,11 +123,20 @@
 
 <script>
 import myBread from "../custom/myBread.vue";
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+import { quillEditor } from "vue-quill-editor";
+
 export default {
+  //局部注册富文本编辑器的组件
   data() {
     return {
       arrStaticparams: [],
       active: "1",
+      headers: {
+        Authorization: localStorage.getItem("token")
+      },
       // id
       // goods_name
       // goods_price
@@ -120,8 +151,9 @@ export default {
         goods_number: "",
         goods_weight: "",
         goods_introduce: "",
-        pics: "",
-        attrs: ""
+        goods_cat: "",
+        pics: [],
+        attrs: []
       },
       arrDyparams: [],
       // 级联选择器绑定的数据
@@ -138,6 +170,33 @@ export default {
     this.getGoodCate();
   },
   methods: {
+    //添加商品
+    async addGoods() {
+      //将数组转换成以逗号分割的字符串
+      this.form.goods_cat = this.selectedOptions.join(",");
+
+      const res = await this.$http.post(`goods`, this.form);
+      console.log(res);
+    },
+    //图片上传的相关方法
+    //data.tmp_path
+    //response.data.tmp_path
+    handlePreview(file) {},
+    handleRemove(file) {
+      console.log("移除");
+      //findIndex符合条件的索引进行返回
+      let Index = this.form.pics.findIndex(item => {
+        return item.pic === file.response.data.tmp_path;
+      });
+      this.form.pics.splice(Index, 1);
+      console.log(this.form.pics);
+    },
+    handleSuccess(file) {
+      console.log("成功");
+      this.form.pics.push({
+        pic: file.data.tmp_path
+      });
+    },
     // 点击不同的tabs触发方法
     async tabChange() {
       if (this.active === "1") {
@@ -174,12 +233,16 @@ export default {
       this.options = res.data.data;
     }
   },
-  components: { myBread }
+  components: { myBread, quillEditor }
 };
 </script>
 
 <style>
 .alert {
   margin-top: 30px;
+}
+
+.ql-editor {
+  min-height: 250px;
 }
 </style>
